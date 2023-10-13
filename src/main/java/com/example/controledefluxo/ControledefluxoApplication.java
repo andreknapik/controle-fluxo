259927a550041c3cd0445a9d58a6d6f85959c6b5
André Knapik <82775531+andreknapik@users.noreply.github.com>
2023-10-13 16:40:33 -0300

    configuracoes iniciais projeto DIO

---
+++ src/main/java/com/example/controledefluxo/ControledefluxoApplication.java
@@ -0,0 +1,13 @@
+package com.example.controledefluxo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControledefluxoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ControledefluxoApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/controledefluxo/ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+package com.example.controledefluxo;
+
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String message) {
+        super(message);
+    }
+}
